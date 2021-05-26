05473f0b80b942410a776021d6d21f8fa664c104
Luc Moutote <75418735+Luchorigami@users.noreply.github.com>
2021-05-26 10:14:00 +0200

    Add files via upload

---
+++ expect_etas.py
@@ -0,0 +1,87 @@
+import numpy as np
+import matplotlib.pyplot  as plt
+import glob
+import time
+import scipy.stats as sc
+from matplotlib.gridspec import GridSpec
+plt.close('all')
+
+# Load Etas estimation file's paths
+fittedpaths = glob.glob('/home/moutoteluc/Documents/stageM2_marsan/review/newMc/fitetas/alphafree/*.npz')
+
+
+for ipath in range(len(fittedpaths)):
+    # Load estimation
+    fittedETAS      = np.load(fittedpaths[ipath])
+    A               = fittedETAS.f.A
+    c               = fittedETAS.f.c
+    p               = fittedETAS.f.p
+    alpha           = fittedETAS.f.alpha
+    mu              = fittedETAS.f.mu
+    mc              = fittedETAS.f.mc
+    IDmain          = fittedETAS.f.IDmain
+    ID              = fittedETAS.f.ID
+    tday            = fittedETAS.f.tday
+    mag             = fittedETAS.f.mag
+
+    print('\nMainshock %d / %d n°%d\n'%(ipath+1,len(fittedpaths),int(IDmain)))
+     
+    # Mainshock caracteristics 
+    imain           = np.where(ID==IDmain)[0]    
+    tmain           = tday  [imain]
+    magmain         = mag   [imain]
+    
+    # Length of foreshock window in days
+    fw              = 20 
+    twinends        = np.concatenate( [ np.flip( np.arange(tmain,tday[0],-1)) , np.arange(tmain+1,tday[-1],1)] ) 
+  
+    Naft            = []
+    bgd             = []
+    Nobs            = []
+
+    for twe in twinends:
+        # Integration of ETAS formula for events before the window
+        i_out       = np.where(tday < twe-fw)[0]
+        t_out       = tday[i_out]
+        m_out       = mag [i_out]
+        Naft_out    = np.sum( A*np.exp(alpha*(m_out-mc))/(1-p) * ((twe-t_out + c)**(1-p) - (twe-fw-t_out + c)**(1-p)) )
+
+        # Integration of ETAS formula for event in the window
+        i_in        = np.where((tday >= twe-fw)*(tday<twe))[0]
+        t_in        = tday[i_in]
+        m_in        = mag [i_in]
+        Naft_in     = np.sum( A*np.exp(alpha*(m_in-mc))/(1-p)  * ((twe-t_in + c)**(1-p) -c**(1-p)) )
+
+        # Number of aftershock = in + out
+        Naft        .append(Naft_out + Naft_in)   
+
+        # Real observed number
+        Nobs        .append(len(i_in))
+
+    Nobs            = np.array(Nobs) 
+    
+    # Expected number of event = Naft + background seismicity
+    bgd             = mu*fw
+    Nexpect         = np.array(Naft) + np.array(bgd)
+      
+    # Evaluate the probabilty of observing at least Nobs alonf a poisson lax of mean Nexpect
+    prob            = sc.poisson.cdf(Nobs,Nexpect)
+    prob            = 1-prob
+    
+    #case Nobs=1 => p(Netas>=Nobs) = 1 
+    prob[np.where(Nobs==0)[0]]=1
+
+    # Center time axis on the mainshock 
+    twinendsmain    = twinends-tmain
+    
+    # position of the foreshock window (i.e. the window just before the mainshsock ) 
+    iwin            = np.where(twinendsmain == 0)[0]
+    probw           = prob[iwin]
+
+    if len(probw)==0:
+        probw = 1
+
+    np.savez('alphafree/%s.expect.alf'%IDmain,prob=prob,Nexpect=Nexpect,probw=probw,Naft=Naft,bgd=bgd,twinendsmain=twinendsmain,IDmain=IDmain,tday=tday,mag=mag,tmain=tmain,magmain=magmain)
+
+
+
